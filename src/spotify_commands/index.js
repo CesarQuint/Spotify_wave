21fd2fe3f52597e021768cff1e501cb53bd7a935
CesarQuint <pedrocesarquintero001@gmail.com>
2024-11-08 13:23:51 -0600

    functions related to spotify's API

--- src/spotify_commands/index.js
+++ src/spotify_commands/index.js
@@ -1,15 +1,24 @@
-import { exec } from "child_process";
+import axios from "axios";
 
-export function playMusic() {
-  exec("spotify play", (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`Stderr: ${stderr}`);
-      return;
-    }
-    console.log(`Spotify CLI Output:\n${stdout}`);
-  });
+export async function sendAuthRequest(clientId, clientSecret) {
+  const url = "https://accounts.spotify.com/api/token";
+  try {
+    const response = await axios.post(
+      url,
+      new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: clientId,
+        client_secret: clientSecret,
+      }),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching Spotify token:", error);
+    throw error;
+  }
 }
