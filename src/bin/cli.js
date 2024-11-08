12da9a3bd36e8edae71e8c55aaae524ae5c228ad
CesarQuint <pedrocesarquintero001@gmail.com>
2024-11-08 13:24:09 -0600

    Update dependencies

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
-
 import { Command } from "commander";
-import authenticate from "../src/cli_commands/index.js";
+
+import { authenticate } from "../cli_commands/index.js";
 
 const program = new Command();
 
 program
+  .name("spotify-cli")
+  .description("CLI to interact with Spotify")
+  .version("1.0.0")
+  .action(() => {
+    console.log("Hello!");
+  });
+
+program
+  .option("auth ,gui")
   .command("auth")
   .description("Authenticate with your Spotify account")
   .action(async () => {
